db8c74c3d44e395225f7c1b6fc9a636e81107b28
SendoDI <diogo.chou@hotmai.com>
2019-12-05 17:07:27 +0000

    t

--- Mobile/app/src/main/java/pt/isec/mindunlocker/MainActivity.java
+++ Mobile/app/src/main/java/pt/isec/mindunlocker/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         getViews();
 
+        leaderContainer.cleanRanking();
         leaderContainer.getLeaderBoard();
         leaderContainer.displayData();
 
