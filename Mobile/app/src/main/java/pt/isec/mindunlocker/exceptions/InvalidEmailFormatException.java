b07a2ccf69ef25e934888a1ad3759eeb403ec380
theknzz <joaquimpersonalmail@gmail.com>
2019-12-05 01:21:11 +0000

    i'm a superUser cleaning old files

--- Mobile/app/src/main/java/pt/isec/mindunlocker/exceptions/InvalidEmailFormatException.java
+++
@@ -1,7 +0,0 @@
-package pt.isec.mindunlocker.exceptions;
-
-public class InvalidEmailFormatException extends Exception {
-    public InvalidEmailFormatException() {
-        super("Invalid email format!\nExample: (example@mindUnlocker.com)");
-    }
-}
